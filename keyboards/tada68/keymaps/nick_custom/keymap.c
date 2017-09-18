42c05f41e81726542356ff07f1f82e8b9e5a1217
NickSadjoli <nick.sadjoli@gmail.com>
2017-09-18 22:26:32 +0800

    Swapped Backspace and Backslash

--- keyboards/tada68/keymaps/nick_custom/keymap.c
+++ keyboards/tada68/keymaps/nick_custom/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------'
    */
 [_WL] = KEYMAP_ANSI(
-  KC_GESC,   KC_1,   	KC_2, 		KC_3,   	KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,   	KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,	KC_INS, \
-  KC_TAB,  	KC_Q,   	KC_W,   	KC_E,   	KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,   	KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_DEL, \
+  KC_GESC,   KC_1,   	KC_2, 		KC_3,   	KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,   	KC_0,   	KC_MINS,	KC_EQL, 	KC_BSLS,	KC_INS, \
+  KC_TAB,  	KC_Q,   	KC_W,   	KC_E,   	KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,   	KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSPC,	KC_DEL, \
   EW_CAPS, 	KC_A,   	KC_S,   	KC_D,   	KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,   	KC_SCLN,	KC_QUOT,		    KC_ENT,				KC_PSCR, \
   KC_LSFT,  KC_Z,   	KC_X,   	NP_C,   	KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,		KC_RSFT , 	  KC_UP,		    M_GUI, \
   KC_LCTL, 	M_GUI,		KC_LALT,                					KC_SPACE,				     		MO(_WU),	KC_RALT, 	SW_RCTRL, 	KC_LEFT, 	    KC_DOWN, 	    KC_RGHT),
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------'
    */
 [_LL] = KEYMAP_ANSI(
-  KC_GESC,   KC_1,   	KC_2, 		KC_3,   	KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,   	KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,	KC_INS, \
-  KC_TAB,  	KC_Q,   	KC_W,     KC_E,   	KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,   	KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_DEL, \
+  KC_GESC,   KC_1,   	KC_2, 		KC_3,   	KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,   	KC_0,   	KC_MINS,	KC_EQL, 	KC_BSLS,	KC_INS, \
+  KC_TAB,  	KC_Q,   	KC_W,     KC_E,   	KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,   	KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSPC,	KC_DEL, \
   EL_CAPS, 	KC_A,   	KC_S,   	KC_D,   	KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,   	KC_SCLN,	KC_QUOT,		    KC_ENT,				KC_PSCR, \
   KC_LSFT,  KC_Z,   	KC_X,   	NP_C,   	KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,		KC_RSFT ,     KC_UP,		    M_GUI, \
   KC_LCTL, 	M_GUI,	KC_LALT,                					KC_SPACE,				     			MO(_LU),	KC_RALT, 	SW_RCTRL, 	KC_LEFT, 	    KC_DOWN, 	    KC_RGHT),
