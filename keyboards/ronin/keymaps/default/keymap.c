8863f46b5da8ec0355e849294e7daa8fa82aad0f
NickSadjoli <nick.sadjoli@gmail.com>
2017-11-28 00:40:49 +0800

    Finalized keymap for preference usage for now.

--- keyboards/ronin/keymaps/default/keymap.c
+++ keyboards/ronin/keymaps/default/keymap.c
@@ -23,8 +23,10 @@
 
 
 #define MACRO_GUI           99
+#define BACK_TAB            0
 
 #define M_GUI  M(MACRO_GUI)
+#define B_TAB  M(BACK_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _WL: Windows Layer (Layer for use in Windows OS, Default Layer when first flashed)
@@ -42,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_ML] = KEYMAP(
   KC_GESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,    KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,  KC_BSLS,    KC_GRV ,\
-    KC_TAB  ,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_INS,     KC_DEL , \
+    KC_TAB  ,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_DEL,     KC_BSPC , \
     KC_LCTL ,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    KC_ENT ,  KC_INS , \
     KC_LSFT ,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT  ,  KC_UP,  KC_PSCR, \
-  KC_CAPS,  M_GUI,  KC_LALT,   KC_SPACE ,  MO(_U1),  MO(_U2),   KC_BSPC,  KC_RCTRL, KC_RALT,   KC_LEFT,  KC_DOWN,  KC_RGHT) ,
+  KC_CAPS,  KC_LGUI,  KC_LALT,   KC_SPACE ,  MO(_U1),  MO(_U2),   KC_BSPC,  KC_RCTRL, KC_RALT,   KC_LEFT,  KC_DOWN,  KC_RGHT) ,
 
 [_U1] = KEYMAP(
   _____,  _____,  _____,  _____,  _____,  _____,  _____,  _____,    KC_PSLS,  KC_PAST,  _____,  _____,  _____,  _____,    _____ ,\
@@ -80,6 +82,18 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           }
           else {
             return MACRO(U(LGUI), END);
+          }
+
+          break;
+        }
+
+        case BACK_TAB:{
+          if (record->event.pressed){
+            //when key is PRESSED
+            return MACRO(D(LSHIFT), D(TAB), END);
+          }
+          else {
+            return MACRO(U(LSHIFT), U(TAB), END);
           }
 
           break;
